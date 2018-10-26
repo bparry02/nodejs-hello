a6c02bc5de56cc33416550c294347ad47caa8e6d
Bryan Parry <bparry@redhat.com>
2018-10-26 10:47:57 -0400

    Add hostname to hello message

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const http = require('http');
 
-//const hostname = '127.0.0.1';
-
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0'
+
+var os = require("os");
+var hostname = os.hostname();
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  res.end(`Hello World from ${hostname}\n`);
 });
 
 server.listen(port, ip, () => {
