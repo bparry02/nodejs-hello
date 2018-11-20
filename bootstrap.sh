e65a1da3ee731f746533006565ed8eaff4f303de
Bryan Parry <bparry@redhat.com>
2018-11-20 11:56:20 -0500

    clarify bootstrap script output

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-echo "Preparing to launch application..."
+echo -e "\n\n-- BEGIN CUSTOM RUN SCRIPT --"
+echo -e "\tPreparing to launch application..."
+echo -e "-- END CUSTOM RUN SCRIPT --\n\n"
 
